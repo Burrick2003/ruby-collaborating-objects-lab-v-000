cea58cac7ebc61d7809e5c3c81003aa2b8b1efa5
Blakeley Bond <burrick2003@yahoo.com>
2017-12-13 03:10:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,36 +13,17 @@ attr_accessor :name, :songs, :all
   def self.all
     @@all
   end
-
-  def save
-    @@all << self
-
-  end
-
-  def print_songs
-    self.songs.each do |song|
-      puts song.name
-    end
-  end
-
-
   def self.find_or_create_by_name(name)
-      self.find(name) ? self.find(name) : self.create(name)
-    end
-
-    def self.find(name)
-      self.all.find {|artist| artist.name == name }
-    end
-
-
-    def self.create(name)
+    if @@all.detect do |artist|
+        name == artist.name
+      end
+      @@all.detect do |artist|
+        name == artist.name
+      end
+    else
       self.new(name).tap {|artist| artist.save}
     end
-
-    def save
-      @@all << self
-    end
-
+  end
   def print_songs
     @songs.each do |song|
       puts song.name
