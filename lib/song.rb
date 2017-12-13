cc712aae8937a610a4b1398779a8d1280850b677
Blakeley Bond <burrick2003@yahoo.com>
2017-12-13 03:20:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,12 +11,14 @@ attr_accessor :name, :artist
   def self.new_by_filename(filename) #filename is artist,name,genre whereas init is name,artist,genre
       array = filename.split(" - ")
       newsong = self.new(array[1])
-      newsong.artist = Artist.find_or_create_by_name(array[0])
+      new_song.artist_name = array[0]
 
       @@all << newsong
       #binding.pry
     end
-
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+      end
 
 
 end
